6261e4b035c295c7ea62f61682a2e8fed41aec97
Tyler Geerdts <tygee713@gmail.com>
2022-11-08 16:59:44 -0500

    Added empty functions with descriptions

--- src/game.js
+++ src/game.js
@@ -1,7 +1,28 @@
 import { init, GameLoop } from '../lib/kontra.min.mjs'
 import MainScene from './scenes/main.js'
+import Sentence from './objects/sentence.js'
 
 const { canvas } = init()
+
+let currentScene = 0
+
+// Moves from the intro scene to the main scene and resets the round number
+// Called when user presses the start button on the initial scene
+const startGame = () => {
+  currentScene = 1
+}
+
+// Moves from the main scene to the ending scene
+// Called when the time runs out or all the sentences have been completed
+const endGame = () => {
+  currentScene = 2
+}
+
+// Goes back to the starting scene
+// Called when the user presses the try again button on the ending scene
+const restartGame = () => {
+  currentScene = 0
+}
 
 const loop = GameLoop({
   update: (dt) => {
--- src/scenes/main.js
+++ src/scenes/main.js
@@ -6,10 +6,41 @@ import ThoughtBubble from '../objects/thoughtBubble.js'
 import Words from '../objects/words.js'
 
 const { canvas } = init()
+let currentSentence = []
+let currentWords = []
+let roundNumber = 0
+let sentences = ["This is a test"]
+
+// Gets the next sentence in the list and creates the word bank
+// Called after the sentence is completed
+const showNextSentence = () => {
+  roundNumber++
+
+  currentSentence = Sentence(sentences[roundNumber])
+  currentWords = Words(sentences[roundNumber])
+}
+
+// Determine if the filled in sentence is correct
+// Called when all of the words are filled in or the time runs out
+const completeSentence = () => {
+  
+}
+
+// Changes the void in the sentence with the word that was selected
+// Called when a void and a word are both selected
+const fillInWord = () => {
+
+}
+
+// Removes a word from the selected void and returns it to the word bank
+// Called when user presses an undo button
+const clearWord = () => {
+
+}
 
 const scene = Scene({
   id: 'main',
-  objects: [Friend, Player, ThoughtBubble, ...Sentence, ...Words],
+  objects: [Friend, Player, ThoughtBubble, ...currentSentence, ...currentWords],
 })
 
 export default scene
