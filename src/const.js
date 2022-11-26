85c42384c0de8c734ad86c9b5af572b9cddce9bc
Tyler Geerdts <tygee713@gmail.com>
2022-11-25 22:50:24 -0500

    Added sentences list

---
+++ src/const.js
@@ -0,0 +1,52 @@
+export const sentences = [
+  {
+    sentence: ['Armchair', 'psychiatrist'],
+    emptyPositions: [1],
+    options: ['quarterback', 'chef', 'doctor', 'psychiatrist']
+  },
+  {
+    sentence: ['Fits like a', 'glove'],
+    emptyPositions: [1],
+    options: ['sock', 'paper bag', 'mitten', 'glove']
+  },
+  {
+    sentence: ['Your', 'goose', 'is cooked'],
+    emptyPositions: [1],
+    options: ['barbecue', 'cookie', 'turkey', 'goose']
+  },
+  {
+    sentence: ['Don\'t put all your', 'eggs', 'in one', 'basket'],
+    emptyPositions: [1, 3],
+    options: ['bugs', 'babies', 'hole', 'cup', 'eggs', 'basket']
+  },
+  {
+    sentence: ['Money', 'doesn\'t grow on', 'trees'],
+    emptyPositions: [0, 2],
+    options: ['candy', 'honey', 'rocks', 'venus', 'money', 'trees']
+  },
+  {
+    sentence: ['From the', 'frying pan', 'into the', 'fire'],
+    emptyPositions: [1, 3],
+    options: ['freezer', 'kettle', 'clouds', 'dirt', 'frying pan', 'fire']
+  },
+  {
+    sentence: ['Elvis', 'has left the', 'building'],
+    emptyPositions: [0, 2],
+    options: ['obama', 'the pope', 'white house', 'vatican', 'elvis', 'building']
+  },
+  {
+    sentence: ['When you lie with', 'dogs', ', you catch', 'fleas'],
+    emptyPositions: [1, 3],
+    options: ['squirrels', 'llamas', 'diarrhea', 'a cold', 'dogs', 'fleas']
+  },
+  // {
+  //   sentence: ['So', 'quiet', 'you could hear a', 'pin', 'drop'],
+  //   emptyPositions: [1, 3, 4],
+  //   options: []
+  // },
+  // {
+  //   sentence: [],
+  //   emptyPositions: [],
+  //   options: []
+  // }
+]
